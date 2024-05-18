37fd1392437f7e41cc54276fe9baaa9b27a5b45f
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-05-18 16:01:22 -0300

    style: apply linting

--- src/order/repository/order.repository.interface.ts
+++ src/order/repository/order.repository.interface.ts
@@ -1,6 +1,4 @@
 import { OrderResponseDto } from '../dto/order-reponse.dto';
-import { ProductOrderDto } from '../dto/product-order.dto';
-import { ProductPriceDto } from '../dto/product-price.dto';
 import { OrderWithAddressDto } from '../dto/order-with-address.dto';
 
 export abstract class IOrderRepository {
