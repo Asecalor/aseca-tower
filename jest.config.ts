c2a235d12f8ac4fc7960125652ca67501819cedd
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-05-29 13:55:13 -0300

    feat: add jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,9 @@ const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     collectCoverage: true,
-    rootDir: '../',
     moduleNameMapper: {
         '^src/(.*)$': '<rootDir>/src/$1',
+        '^test/(.*)$': '<rootDir>/test/$1',
       },
 }
 
